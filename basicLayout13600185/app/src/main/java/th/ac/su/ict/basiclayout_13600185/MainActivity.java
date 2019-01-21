1b113a78789d00b6f812398e743dcb1a0f78559c
natsinee <thanannirat_n@silpakorn.edu>
2019-01-21 16:08:07 +0700

    layout01

---
+++ basicLayout13600185/app/src/main/java/th/ac/su/ict/basiclayout_13600185/MainActivity.java
@@ -0,0 +1,13 @@
+package th.ac.su.ict.basiclayout_13600185;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.relative_layout_3);
+    }
+}
