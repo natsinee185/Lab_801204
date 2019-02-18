5fcda896bd98a341873787a63ffaf2b23ede81bb
natsinee <thanannirat_n@silpakorn.edu>
2019-02-18 10:04:32 +0700

    update

--- helloView/app/src/main/java/th/ac/su/ict/helloview/MainActivity.java
+++ helloView/app/src/main/java/th/ac/su/ict/helloview/MainActivity.java
@@ -1,4 +1,4 @@
-package th.ac.su.ict.basiclayout_13600185;
+package th.ac.su.ict.helloview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.relative_layout_6);
+        setContentView(R.layout.activity_main);
     }
 }
